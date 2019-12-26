89bd36f1ab5aa95001a70802d438a0a8a712085e
jonathan blum <45519651+luliblum@users.noreply.github.com>
2019-12-26 14:40:00 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 . .env
-sudo docker-compose up -d
+docker-compose up -d
 sleep 4 
-sudo docker exec -it pizza-express_app_1 npm test
+docker exec -it pizza-express_app_1 npm test
 if [[ $? != "0" ]]; then
 	echo "error in test"
 	exit 1
 fi
 
-sudo docker-compose build
+docker-compose build
 if [[ $? != "0" ]]; then
 	echo "error in build"
 	exit 1
 fi
 
-sudo docker tag "$USERNAME/$IMAGE:$VER" "$USERNAME/$IMAGE:latest"
+docker tag "$USERNAME/$IMAGE:$VER" "$USERNAME/$IMAGE:latest"
 
 if [[ $? != "0" ]]; then
 	echo "error in giving tag"
@@ -23,9 +23,9 @@ if [[ $? != "0" ]]; then
 fi
 sleep 1
 
-sudo docker-compose down
+docker-compose down
 sleep 5
-sudo docker-compose up -d
+docker-compose up -d
 if [[ $? != "0" ]]; then
 	echo "error in up"
 	exit 1
@@ -38,16 +38,16 @@ if [[ $response != "200" ]]; then
 	exit 1
 fi
 
-# sudo docker rmi "$USERNAME/$IMAGE:latest"
+# docker rmi "$USERNAME/$IMAGE:latest"
 
 #if [[ $? != "0" ]]; then
 #	echo "error in rm image container"
 #	exit 1
 #fi
 
-sudo docker login -u $USERNAME -p $PASSWORD 
+docker login -u $USERNAME -p $PASSWORD 
 
-sudo docker push "$USERNAME/$IMAGE:$VER"
+docker push "$USERNAME/$IMAGE:$VER"
 if [[ $? != "0" ]]; then
 	echo "error in push image"
 	exit 1
@@ -55,7 +55,7 @@ fi
 
 sleep 5
 
-sudo docker push "$USERNAME/$IMAGE:latest"
+docker push "$USERNAME/$IMAGE:latest"
 
 if [[ $? != "0" ]]; then
 	echo "error by pushing the latest name tag"
